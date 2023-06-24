5f0b4e1013797991eae156bd6b419f5b7f5a51e5
gizzSaw <gizzsaw@gmail.com>
2023-06-24 22:45:54 +0300

    forecast

--- scripts/modules/render.js
+++ scripts/modules/render.js
@@ -30,9 +30,6 @@ export const renderWidgetToday = (widget, data) => {
 }
 
 export const renderWidgetOther = (widget, data) => {
-        console.log(data)
-
-
         widget.insertAdjacentHTML(
         'beforeend', 
         `
@@ -60,39 +57,59 @@ export const renderWidgetOther = (widget, data) => {
     )
 }
 
-export const renderWidgetForecast = (widget) => {
-        widget.insertAdjacentHTML(
-        'beforeend', 
-        `
-        <ul class="widget__forecast">
-            <li class="widget__day-item">
-            <p class="widget__day-text">ср</p>
-            <img class="widget__day-img" src="./icon/02d.svg" alt="Погода">
-            <p class="widget__day-temp">18.4°/13.7°</p>
-            </li>
-            <li class="widget__day-item">
-            <p class="widget__day-text">чт</p>
-            <img class="widget__day-img" src="./icon/03d.svg" alt="Погода">
+export const renderWidgetForecast = (widget, data) => {
+        console.log(data)
+
+        const widgetForecast = document.createElement('ul')
+        widgetForecast.className = 'widget__forecast'
+        widgetForecast.append(widgetForecast)
+
+        const forecastData = data
+
+        const items = forecastData.map((item) => {
+            const widgetDayItem = document.createElement('li')
+            widgetDayItem.className = 'widget__day-item'
+
+            widgetDayItem.insertAdjacentElement('beforebegin', 
+            `
+            <p class="widget__day-text">${item.dayOfWeek}</p>
+             <img class="widget__day-img" src="./icon/${item.weatherIcon}.svg" alt="Погода">
             <p class="widget__day-temp">17.3°/11.3°</p>
-            </li>
-            <li class="widget__day-item">
-            <p class="widget__day-text">пт</p>
-            <img class="widget__day-img" src="./icon/04d.svg" alt="Погода">
-            <p class="widget__day-temp">16.5°/10.9°</p>
-            </li>
-            <li class="widget__day-item">
-            <p class="widget__day-text">сб</p>
-            <img class="widget__day-img" src="./icon/01d.svg" alt="Погода">
-            <p class="widget__day-temp">18.6°/12.5°</p>
-            </li>
-            <li class="widget__day-item">
-            <p class="widget__day-text">вс</p>
-            <img class="widget__day-img" src="./icon/03d.svg" alt="Погода">
-            <p class="widget__day-temp">17.3°/11.2°</p>
-            </li>
-        </ul>
-        `
-    )
+            `)
+
+            return widgetDayItem
+        })
+
+        widgetForecast.append(...items)
+
+
+        // <ul class="widget__forecast">
+        //     <li class="widget__day-item">
+        //     <p class="widget__day-text">ср</p>
+        //     <img class="widget__day-img" src="./icon/02d.svg" alt="Погода">
+        //     <p class="widget__day-temp">18.4°/13.7°</p>
+        //     </li>
+        //     <li class="widget__day-item">
+        //     <p class="widget__day-text">чт</p>
+        //     <img class="widget__day-img" src="./icon/03d.svg" alt="Погода">
+        //     <p class="widget__day-temp">17.3°/11.3°</p>
+        //     </li>
+        //     <li class="widget__day-item">
+        //     <p class="widget__day-text">пт</p>
+        //     <img class="widget__day-img" src="./icon/04d.svg" alt="Погода">
+        //     <p class="widget__day-temp">16.5°/10.9°</p>
+        //     </li>
+        //     <li class="widget__day-item">
+        //     <p class="widget__day-text">сб</p>
+        //     <img class="widget__day-img" src="./icon/01d.svg" alt="Погода">
+        //     <p class="widget__day-temp">18.6°/12.5°</p>
+        //     </li>
+        //     <li class="widget__day-item">
+        //     <p class="widget__day-text">вс</p>
+        //     <img class="widget__day-img" src="./icon/03d.svg" alt="Погода">
+        //     <p class="widget__day-temp">17.3°/11.2°</p>
+        //     </li>
+        // </ul>
 }
 
 export const showError = (widget, error) => {
--- scripts/modules/widgetServices.js
+++ scripts/modules/widgetServices.js
@@ -1,4 +1,4 @@
-import { fetchWeather } from './APIService.js'
+import { fetchForecast, fetchWeather } from './APIService.js'
 import { 
     renderWidgetForecast, 
     renderWidgetOther, 
@@ -7,20 +7,27 @@ import {
 } from './render.js'
 
 export const startWidget = async () => {
+    const city = 'Воронеж'
     const widget = document.createElement('div')
     widget.classList.add('widget')
 
-    const dataWeather = await fetchWeather('Воронеж')
+    const dataWeather = await fetchWeather(city)
 
     if(dataWeather.succcess) {
         renderWidgetToday(widget, dataWeather.data)
         renderWidgetOther(widget, dataWeather.data)
     } else {
-        showError()
+        showError(dataWeather.error)
     }
 
+    const dataForecast = await fetchForecast(city)
 
-    renderWidgetForecast(widget)
+    if(dataForecast.succcess) {
+        renderWidgetForecast(widget, dataForecast.data)
+    } else {
+        showError(dataForecast.error)
+    }
+    
 
     return widget
 }
